47e969b109a35c932db2e4d8665c7bd08fbf7939
Hikkiphisan <162010903+Hikkiphisan@users.noreply.github.com>
2024-10-17 11:48:51 +0700

    Initial commit

---
+++ src/FulltimeEmployee.java
@@ -0,0 +1,20 @@
+public class FulltimeEmployee extends Employee implements SalaryCaculation {
+    public double bonusSalary;
+    public double penaltySalary;
+    public double baseSalary;
+
+    public void FulltimeEmployee() {
+    }
+    public FulltimeEmployee(int id_employee, String name, int age, int numberphone, String email, double bonusSalary, double penaltySalary, double baseSalary) {
+        super(id_employee, name, age, numberphone, email);
+        this.bonusSalary = bonusSalary;
+        this.penaltySalary = penaltySalary;
+        this.baseSalary = baseSalary;
+    }
+
+
+    @Override
+    public double salaryCalculation() {
+        return baseSalary + (baseSalary - penaltySalary);
+    }
+}
---
+++ src/ParttimeEmployee.java
@@ -0,0 +1,13 @@
+public class ParttimeEmployee extends Employee implements SalaryCaculation {
+    public int hourforWork;
+
+    public ParttimeEmployee(int id_employee, String name, int age, int numberphone, String email, int hourforWork) {
+        super(id_employee, name, age, numberphone, email);
+        this.hourforWork = hourforWork;
+    }
+
+    @Override
+    public double salaryCalculation() {
+        return hourforWork* 100000;
+    }
+}
